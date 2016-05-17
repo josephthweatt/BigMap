ca8d94a513185b87a602f8e5d590398684163fd9
Joseph Thweatt <josephthweatt@gmail.com>
2016-05-17 13:26:33 -0700

    Started on Android development. The project is built in another folder but I thought I'd commit the app code along with the server code

---
+++ Android/main/java/com/example/joseph/bigmap/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.joseph.bigmap;
+
+import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
