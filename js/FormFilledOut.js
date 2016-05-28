b429170cf45fd05fa99b0be0068ed12abd9642b1
unknown <jathweat@EN4110021214.fulton.ad.asu.edu>
2016-05-28 16:37:54 -0700

    added FormFilledOut.js to check if a form is being filled out

---
+++ js/FormFilledOut.js
@@ -0,0 +1,18 @@
+function formFilledOut() {
+    var elements = document.getElementById("f").elements;
+    var formFilled = true;
+    var missingString = "";
+
+    // go through all form elements EXCEPT the button elements
+    for (var i = 0, element; i < elements.length - 1; i++) {
+        element = elements[i].value;
+        if ((element === "" || element === null) && elements[i].type.localeCompare("submit")) {
+            missingString += "Missing entry at box" + (i + 1) + "\n";
+            formFilled = false;
+        }
+    }
+    if (!formFilled) {
+        window.alert(missingString);
+    }
+    return formFilled;
+}
