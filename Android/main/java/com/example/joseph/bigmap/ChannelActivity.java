72d460fb4efca990a22fe4d87a647d912fe3f1da
Joseph Thweatt <josephthweatt@gmail.com>
2016-06-04 22:25:07 -0700

    Non-working implementation of the map in android
    This map uses fragment to be implemented within the ChannelActivity. It's not displaying though. I'm going to try to use the FragmentActivity and see if that changes anything

--- Android/main/java/com/example/joseph/bigmap/ChannelActivity.java
+++ Android/main/java/com/example/joseph/bigmap/ChannelActivity.java
@@ -4,19 +4,34 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ChannelActivity extends AppCompatActivity{
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ChannelActivity extends AppCompatActivity {
     private static String TAG = "ChannelActivity";
     public static final String PREFS_NAME = "StoredUserInfo";
     SharedPreferences sharedPreferences;
+    FragmentManager fragmentManager;
 
     private String header;
     private Button broadcastButton;
@@ -31,23 +46,34 @@ public class ChannelActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.channel_activity);
 
+        // start map fragment
+        if (savedInstanceState == null) {
+            Fragment mapFragment = new ChannelMap();
+            fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(mapFragment, mapFragment.getTag());
+        }
+
+        // set header title
         channelId = getIntent().getIntExtra("channelId", 0);
         header = "Channel " + channelId;
         ((TextView) findViewById(R.id.channel_header)).setText(header);
 
+        // service intent used to start LocationService
         if (serviceIntent == null) {
             serviceIntent = new Intent(ChannelActivity.this, LocationService.class);
         }
 
+        // set the state of a button, either from shared preferences or "off" by default
         sharedPreferences = getSharedPreferences(PREFS_NAME, 0);
         if (sharedPreferences.contains(header)) {
             broadcasting = sharedPreferences.getBoolean(header, false);
         } else {
             broadcasting = false;
         }
-
         broadcastButton = (Button) findViewById(R.id.channel_button);
         setBroadcastState(broadcasting);
+
         broadcastButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,7 +110,6 @@ public class ChannelActivity extends AppCompatActivity{
         broadcasting = true;
         storeBroadcastState();
         APIHandler.setBroadcastingChannels();
-        // TODO: make sure the service doesn't stop every time the activity refreshes
         // begin locationService if the APIHandler just received its first broadcasting channel
         if (APIHandler.broadcastingChannels.size() == 1) {
             startService(serviceIntent);
@@ -110,5 +135,34 @@ public class ChannelActivity extends AppCompatActivity{
 
         // update the list in the APIHandler
         APIHandler.setBroadcastingChannels();
+    }
+
+    /***************
+     * Map Fragment
+     ***************/
+    public static class ChannelMap extends Fragment implements OnMapReadyCallback  {
+        private GoogleMap mMap;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            SupportMapFragment mapFragment = (SupportMapFragment)
+                    getFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
+            if (ActivityCompat.checkSelfPermission(getContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getContext(),
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+        }
     }
 }
--- Android/main/java/com/example/joseph/bigmap/LocationService.java
+++ Android/main/java/com/example/joseph/bigmap/LocationService.java
@@ -138,13 +138,6 @@ public class LocationService extends Service implements GoogleApiClient.Connecti
                 && ActivityCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         fusedLocation.requestLocationUpdates(
