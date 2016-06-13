2bdd15321ab888be83a2913854b6175e70790c6c
Joseph Thweatt <josephthweatt@gmail.com>
2016-06-12 17:31:20 -0700

    Added basic interface functions to ChannelSocket

--- PHP/ChannelSocket/ChannelSocket.php
+++ PHP/ChannelSocket/ChannelSocket.php
@@ -1,2 +1,34 @@
 <?php
-    
+    use Ratchet\MessageComponentInterface;
+    use Ratchet\ConnectionInterface;
+
+    // TODO: build socket to manage each channel and distribute locations amongst members
+
+    class ChannelSocket implements MessageComponentInterface {
+        protected $clients;
+
+        public function __construct() {
+            $this->clients = \SplObjectStorage;
+        }
+
+        // attaches connections as clients of the socket
+        public function onOpen(ConnectionInterface $conn) {
+            $this->clients->attach($conn);
+        }
+
+        public function onMessage(ConnectionInterface $conn, $msg) {
+            foreach ($this->clients as $client) {
+                if ($client !== $conn) {
+                    $client->send($msg);
+                }
+            }
+        }
+
+        public function onClose(ConnectionInterface $conn) {
+            $this->clients->detach($conn);
+        }
+
+        public function onError(ConnectionInterface $conn, \Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
